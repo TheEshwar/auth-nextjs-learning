03d5b98d3c5ef879a48f8fa8474900c5123ecacc
TheEshwar <deshmukh.eshwar@gmail.com>
2023-12-28 18:50:24 +0530

    Added middleware and profile details

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function UserProfile({ params }: any) {
 	return (
 		<>
 			<h1>User Profile</h1>
             <h3>Params id :- {params.id}</h3>
+			<Link href={'/profile'} >Back</Link>
 		</>
 	);
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,13 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
 	const router = useRouter();
+	const [userData, setUserData] = useState({})
+
 	const handleLogout = async () => {
 		try {
 			const response = await axios.get("/api/users/logout");
@@ -17,10 +20,21 @@ export default function ProfilePage() {
 			toast.error(error.message);
 		}
 	};
+
+	const getUserDetails = async () =>{
+		const res = await axios.get('/api/users/me');
+		console.log("getuserDetails() res :- ", res.data.data.username);
+		setUserData(res.data.data);
+	}
 	return (
 		<>
 			<h1>Profile page</h1>
+			<h3>{userData 
+			? <Link href={`/profile/${userData._id}`} >{userData.username}</Link> 
+			: "no data found"}</h3>
+
 			<button onClick={handleLogout}>Logout</button>
+			<button onClick={getUserDetails}>User Details</button>
 		</>
 	);
 }
